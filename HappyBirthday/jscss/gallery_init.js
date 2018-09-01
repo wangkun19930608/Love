30f2673a9a6f12fc49c6faaf1dd6d4ff2253fdcf
wangkun <wangkun.19930608@qq.com>
2018-09-01 23:27:31 +0800

    fix bug

--- HappyBirthday/jscss/gallery_init.js
+++ HappyBirthday/jscss/gallery_init.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
 	$('#myRoundabout').roundabout({
-		 shape: 'figure8',
+		 //shape: 'figure8',
 		 // shape: 'theJuggler',
 		 // shape: 'waterWheel',//ok
-		   //shape: 'square',//ok
-		  // shape: 'conveyorBeltLeft',
+		   shape: 'square',//ok
+		   //shape: 'conveyorBeltLeft',
 		 minOpacity: 1
 	});
 });

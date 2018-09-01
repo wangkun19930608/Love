e1cabfe38dc5f2afe1190148bee0d261dfdab992
wangkun <1249516837@qq.com>
2018-09-01 17:47:01 +0800

    add her birthday add encrypt js delete useless files add useful  index.html

--- HappyBirthday/jscss/gallery_init.js
+++ HappyBirthday/jscss/gallery_init.js
@@ -1,6 +1,10 @@
 $(document).ready(function() {
 	$('#myRoundabout').roundabout({
 		 shape: 'figure8',
+		 // shape: 'theJuggler',
+		 // shape: 'waterWheel',//ok
+		   //shape: 'square',//ok
+		  // shape: 'conveyorBeltLeft',
 		 minOpacity: 1
 	});
 });
